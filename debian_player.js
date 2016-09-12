32caf3be44c2b585b87474a83be93e0e2a48f86f
pishon <kuczynskilukasz@gmail.com>
2016-09-12 23:10:02 +0200

    writing to fifo using fifo lib

--- debian_player.js
+++ debian_player.js
@@ -1,39 +1,15 @@
 exports.play = function(station) {
     console.log('playing station '+station)
-
-    spawn = require( 'child_process' ).spawn,
-    cmd = spawn("echo 'loadfile http://gr-relay-1.gaduradio.pl/76' > /tmp/mplayer_fifo")
-
-    cmd.stdout.on( 'data', data => {
-        console.log( `stdout: ${data}` );
-    });
-
-    cmd.stderr.on( 'data', data => {
-        console.log( `stderr: ${data}` );
-    });
-
-    cmd.on( 'close', code => {
-        console.log( `child process exited with code ${code}` );
-    });
-
+    const FIFO = require('fifo-js')
+    let fifo = new FIFO('/tmp/mplayer_fifo')
+    fifo.write('loadfile http://gr-relay-1.gaduradio.pl/76')
+    fifo.close()
 }
 
 exports.stop = function() {
     console.log('stopping')
-
-    spawn = require( 'child_process' ).spawn,
-    cmd = spawn("echo 'stop' /tmp/mplayer_fifo")
-
-    cmd.stdout.on( 'data', data => {
-        console.log( `stdout: ${data}` );
-    });
-
-    cmd.stderr.on( 'data', data => {
-        console.log( `stderr: ${data}` );
-    });
-
-    cmd.on( 'close', code => {
-        console.log( `child process exited with code ${code}` );
-    });
-
+    const FIFO = require('fifo-js')
+    let fifo = new FIFO('/tmp/mplayer_fifo')
+    fifo.write('stop')
+    fifo.close()
 }
